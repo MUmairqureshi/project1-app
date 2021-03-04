ae1c8647e7f61a35ccc20b21b49eac594f2c6b35
MUmairqureshi <67610498+MUmairqureshi@users.noreply.github.com>
2021-03-04 06:37:01 -0800

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,14 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello  World/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('test case', () => {
+  //render(<App />);
+  const {getByTest} = render(<App />);
+  const linkElement =  getByText(/ Hello World/i);
   expect(linkElement).toBeInTheDocument();
 });
